3ef6a50922f5d4ccca9dde3587538985024090cc
tobias <tobiasleongzhunmun@gmail.com>
2018-07-08 01:01:34 +0800

    update datetime

--- application.py
+++ application.py
@@ -25,20 +25,23 @@ application.config['SECRET_KEY'] = "hellowtfwtfwtf1234"
 
 # set timezone
 timezone = timezone("Asia/Singapore")
+
 # routes
 @application.route('/', methods=['GET','POST'])
 def index():
 	form = SubmitForm()
+	# this runs only if form is submitted
 	if form.validate_on_submit():
-		time = " ".join([timezone.localize(datetime.datetime.now()).strftime("%A %d-%m-%Y %H:%M:%S"), 
+		# gets UTC time then converts to local time (+8)
+		time = " ".join([datetime.datetime.now().astimezone(timezone).strftime("%A %d-%m-%Y %H:%M:%S"), 
 			"(SGT)"])
+		# converts to json objects using the HTTP request from DataMall
 		incidents = json.loads(requests.request("GET", DATA_URL, headers=headers).text)
+		# each value is an incident
 		values = incidents["value"]
 		return render_template("index.html", values=values, time=time, form=form)
 	else:
 		return render_template("index.html", form=form)
-
-
 
 # run app
 if __name__ == "__main__":
