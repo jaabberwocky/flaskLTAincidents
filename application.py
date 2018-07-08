d54b6810c59ffd3afba10df2edbb368e5746e6ad
tobias <tobiasleongzhunmun@gmail.com>
2018-07-08 21:17:25 +0800

    changed mapbox tile

--- application.py
+++ application.py
@@ -49,4 +49,4 @@ def index():
 
 # run app
 if __name__ == "__main__":
-	application.run(debug=True)
+	application.run()
--- static/leafletJS.js
+++ static/leafletJS.js
@@ -1,6 +1,6 @@
-var mymap = L.map('mapid').setView([1.3521, 103.8198], 9);
+var mymap = L.map('mapid').setView([1.3521, 103.8198], 11);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/v4/mapbox.pencil/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
